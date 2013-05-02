0a96514c4f6acced4f2ce5156dc592a55d49d1f2
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2013-05-02 18:20:27 +0200

    Add missing file to create servo and rgb demo for eval board only

---
+++ digital/rgb/src/rgb.stm32f4.hh
@@ -0,0 +1,30 @@
+#include "ucoolib/arch/arch.hh"
+#include "ucoolib/hal/gpio/gpio.hh"
+#include "ucoolib/hal/usb/usb.hh"
+#include "ucoolib/utils/delay.hh"
+#include "ucoolib/common.hh"
+
+#include <libopencm3/stm32/f4/rcc.h>
+#include <libopencm3/stm32/f4/timer.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
+#include <cstdio>
+
+static enum colors{
+    WHITE = 0,
+    RED = 1,
+    GREEN = 2,
+    BLUE = 3,
+    UNKNOWN = 4
+} color;
+const char * clr[] = { "WHITE", "RED", "GREEN", "BLUE", "UNK"};
+
+static int measure_cnt = 0;
+static int cur_color = WHITE;
+static float results[4] = {0, 0, 0, 0};
+int setup_measure ();
+void tim2_isr(void);
+void sensor_setup(int enable);
+void setup_color(int new_color);
+void setup_input_capture();
+
